184cdaba7c9402b7debd8f241fa4fd0fc183b2b2
Tope Adenekan <adenekantope.com>
2025-09-27 00:16:53 +0100

    feat: add models

---
+++ src/models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    clerKId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      reauired: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    bannerImage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    bio: {
+      type: String,
+      default: "",
+      maxLenght: 160,
+    },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
